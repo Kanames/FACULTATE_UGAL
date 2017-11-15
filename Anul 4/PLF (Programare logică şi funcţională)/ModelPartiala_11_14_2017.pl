03fff09edb0bf72019a76847c39a9c4eb05339b6
Kanames <Kanames72@gmail.com>
2017-11-15 13:56:17 +0200

    Update ModelPartiala_11_14_2017.pl

--- Anul 4/PLF (Programare logică şi funcţională)/ModelPartiala_11_14_2017.pl
+++ Anul 4/PLF (Programare logică şi funcţională)/ModelPartiala_11_14_2017.pl
@@ -1,9 +1,5 @@
 %5 oferte de desktop care sa aiba urmatoarele caracteristici:
-%- producator
-%- procese
-%- dimenMemRam
-%- lista caracteristici numerice,atom,sir de caractere
-
+%desktop(producator, procesor, dimenMemRam, lista[caracteristici numerice,atom,sir de caractere]).
 %Nr 1
 	%1.Dimensiune mem Ram mai mica de o valoare citita de la tasttura
 	%2.Un anumit producator citit de la tastatura
@@ -15,7 +11,7 @@ desktop(asus,i5,8,[f,g,h]).
 desktop(packBell,i5,4,[,y,z]).
 desktop(samsung,i7,16,[t,r,p]).
 
-%punctul 1
+%--punctul 1
 memRamMaiMareDe(Min):-
 desktop(NumePrducator,_,RAM,_),
 RAM > Min,
@@ -26,13 +22,13 @@ desktop(NumePrducator,_,RAM,_),
 RAM < Max,
 write(NumePrducator),nl.
 
-%punctul 2
+%--punctul 2
 descriereProduseProducator(NumeProducator):-
 desktop(NumePrducator,Procesor,RAM,_),
 NumePrducator = NumeProducator,
 write(NumePrducator),tab(3),write(Procesor),nl,write(RAM),nl.
 
-%punctul 3 solutia 1
+%--punctul 3 solutia 1
 caracteristic(Cara):-
 	desktop(NumProd,Proc,Ram,[X|List]),	
         printlist([X|List],Cara,NumProd).
@@ -40,6 +36,6 @@ printlist([],Cara,NumProd):-nl,write('Nu sa gasit caracteristica !'),nl.
 printlist([X|List],Cara,NumProd) :-
     Cara = X ,write(NumProd),nl, 
     printlist(List,Cara,NumProd).
-%punctul 3 solutia 2	
+%--punctul 3 solutia 2	
 	%puteam sa pun la fiecare desktop inca o caracteristica numit cod unic astfel incat sa duc codul unic mai departe
 	%si cand gaseam un match afisam desktop-ul cu cod unic.
